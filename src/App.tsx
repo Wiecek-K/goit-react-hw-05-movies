26ebe30ceb2bfff70f81ed5aa80afa36a5136175
Wiecek-K <krystiandwiecek@gmail.com>
2023-07-16 15:16:53 +0200

    added sharedLayout

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import { Home } from "./pages/Home/Home";
 import { Movies } from "./pages/Movies/Movies";
 import { MoviePage } from "./pages/MoviePage/MoviePage";
 import { Cast } from "./components/Cast/Cast";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { NotFound } from "./pages/NotFound/NotFound";
 import { Reviews } from "./components/Reviews/Reviews";
+import { SharedLayout } from "./components/SharedLayout/SharedLayout";
 function App() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
-      </nav>
+    <SharedLayout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
@@ -22,7 +19,7 @@ function App() {
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </div>
+    </SharedLayout>
   );
 }
 
---
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import { Link } from "react-router-dom";
+
+interface SharedLayoutProps {
+  children: ReactNode;
+}
+
+export const SharedLayout = ({ children }: SharedLayoutProps) => (
+  <>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/movies">Movies</Link>
+    </nav>
+    {children}
+  </>
+);
